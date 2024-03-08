232d1de2623687186e89beae5abe26495754ad0b
Fitrie <mfitrie78@gmail.com>
2024-03-08 16:00:45 +0800

    feat: make transaction page and transaction item

--- src/app/transaction/page.tsx
+++ src/app/transaction/page.tsx
@@ -1,7 +1,95 @@
+"use client"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Icon } from '@iconify/react';
+import { useState } from "react";
+import TransactionItem from "@/components/transaction-item"
+
+
 export default function Transaction() {
+  // const [open, setOpen] = useState(false);
+  // const [value, setValue] = useState("");
+
+  // const frameworks = [
+  //   {
+  //     value: "next.js",
+  //     label: "Next.js",
+  //   },
+  //   {
+  //     value: "sveltekit",
+  //     label: "SvelteKit",
+  //   },
+  //   {
+  //     value: "nuxt.js",
+  //     label: "Nuxt.js",
+  //   },
+  //   {
+  //     value: "remix",
+  //     label: "Remix",
+  //   },
+  //   {
+  //     value: "astro",
+  //     label: "Astro",
+  //   },
+  // ];
+
+
   return (
     <div style={{ height: "90vh", }}>
-      Transaction Page
+      <div
+        className="container mx-auto grid grid-rows-2 text-white"
+        style={{ height: "20%", backgroundColor: "#26C165",}}
+      >
+        <div className="flex items-center border-b-1">
+          <h1 className="font-bold text-xl">TH Budget Tracker</h1>
+        </div>
+        <div>
+          <span>Transaction History</span>
+          {/* TODO: use Select component for sorting */}
+        </div>
+      </div>
+
+      <div
+        className="container mx-auto py-4 overflow-auto" 
+        style={{ height: "80%", maxHeight: "80%" }}
+      >
+        <Card>
+          <CardContent className="px-2">
+            {
+              Array(40).fill(null).map((item, index) => (
+                <TransactionItem
+                  key={index}
+                  expenseName="Pen"
+                  date="7 Mar 2024 - 11:43 AM"
+                  amount={60}
+                />
+              ))
+            }
+          </CardContent>
+        </Card>
+
+      </div>
     </div>
   );
 }
---
+++ src/components/transaction-item.tsx
@@ -0,0 +1,37 @@
+import {
+    CardDescription,
+} from "@/components/ui/card"
+import { Icon } from '@iconify/react';
+
+type TypeTransactionItem = {
+    expenseName: string,
+    date: string,
+    amount: number,
+}
+
+export default function TransactionItem({
+    expenseName,
+    date,
+    amount
+}: TypeTransactionItem) {
+    return (
+        <div className="py-4 grid grid-cols-12 items-center gap-3 border-b-[1px] border-black">
+            <div className="col-span-2">
+                <div
+                    className="border-solid border-2 border-black rounded-full p-2"
+                    style={{ borderColor: "#26C165" }}
+                >
+                    <Icon className='text-xl' icon="ph:coin" />
+                </div>
+            </div>
+            <div className="col-span-7">
+                <span className="font-bold">{ expenseName }</span>
+                {/* <CardDescription className="text-xs">7 Mar 2024 - 11:43 AM</CardDescription> */}
+                <CardDescription className="text-xs">{ date }</CardDescription>
+            </div>
+            <div className="col-span-3 flex flex-col text-right">
+                <span className="font-bold">RM { amount }</span>
+            </div>
+        </div>
+    );
+}
